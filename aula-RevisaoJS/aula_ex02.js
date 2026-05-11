72f48917248ac7e8fe536b00023466b04809d119
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2026-05-11 22:37:15 +0000

    ex 2 feito

--- aula-RevisaoJS/aula_ex02.js
+++ aula-RevisaoJS/aula_ex02.js
@@ -1,4 +1,25 @@
-function saudacao(){
+/*function saudacao(nome){
 
+     return 'Olá,' + nome + "!";
     
+}   
+
+console.log(saudacao("Layo"));
+
+const soma = function (num1, num2){
+    
+    return num1 + num2
+
 }
+
+console.log(soma(33.5,33.5))
+
+*/
+const multiplicacao = (x, y) => x * y
+
+console.log(multiplicacao(5,4))
+
+
+const divisao = (x,y) => x / y;
+
+console.log(divisao(2,4))
---
+++ aula-RevisaoJS/aula_ex3.js
@@ -0,0 +1,6 @@
+const pessoa = {
+    nomeUsuario: 'layomendes',
+    idade: 19,
+    email: 'layomendes@unipam.edu.br'
+    usuario: 'aluno'
+}
