69a2826011034cc7e01f9158c4ab8e018ce77854
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2026-05-11 23:24:24 +0000

    aula feita

---
+++ aula-RevisaoJS/aula_ex05.js
@@ -0,0 +1,28 @@
+function exibirInfoProduto(){
+    return `Produto: ${produto.nome},
+    Preço: ${produto.preco.toFixed(2)},
+    Estoque: ${exibirInfoProduto.estoque} unidade  `
+
+
+}
+
+const produto1 = {
+    nome: 'notebook',
+    preco: 3400.00,
+    estoque: 10,
+}
+const produto2 = {
+    nome: 'a',
+    preco: 3400.00,
+    estoque: 10,
+}
+
+const produto3 = {
+    nome: 'nodtebook',
+    preco: 3400.00,
+    estoque: 10,
+}
+
+
+
+
