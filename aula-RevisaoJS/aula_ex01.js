a5d3f96cb4487c45eea8f218b6a2784d3eea0f4f
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2026-05-11 22:16:41 +0000

    aula 11-05

---
+++ aula-RevisaoJS/aula_ex01.js
@@ -0,0 +1,36 @@
+//Exemplo 01
+
+//Usando var (escopo global)
+
+var nome = "Layo";
+console.log(nome);
+
+
+if(true){
+    var nome = 'Gabi'
+        console.log(nome)
+
+
+    }
+
+let idade = 35;
+console.log(idade);
+
+if(true){
+    let idade = "maria"
+    console.log(idade)
+
+}
+console.log(nome)
+
+const pi = 3.14
+console.log(pi)
+
+if(true){
+    const pi = 3.14149;
+    console.log(pi);
+
+}
+
+
+console.log(pi)
